8ad58803054f41d1658d1bc3d685373479e083c1
wndudzz6 <osmzxcvb@naver.com>
2025-12-16 16:41:59 +0900

    init : WinDO Spring Boot backend

---
+++ src/main/java/com/hightecher/windo/WinDoApplication.java
@@ -0,0 +1,13 @@
+package com.hightecher.windo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hightecher/windo/WinDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hightecher.windo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
