205d54ed9225ae939d73eb4d46794056d7e1eef1
O ju yeong <osmzxcvb@naver.com>
2026-01-02 16:01:41 +0900

    cors허용

---
+++ src/main/java/com/hightecher/windo/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.hightecher.windo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // 모든 origin 허용
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false); // 중요!
+    }
+}
